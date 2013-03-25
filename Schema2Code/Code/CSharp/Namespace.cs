ad16fa8e87b76cb2977ca5b7d8bc839129f89135
Bas Verburg <b.verburg@codecast.nl>
2013-03-25 17:34:02 +0100

    Implemented namespace, still needs validation

--- Schema2Code/Code/CSharp/Namespace.cs
+++ Schema2Code/Code/CSharp/Namespace.cs
@@ -7,10 +7,23 @@ namespace Schema2Code.Code.CSharp
 {
     public class Namespace : INamespace
     {
+        private String name;
+
         public string Name
         {
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+            get { return name; }
+            set
+            {
+                if (Validate(value))
+                {
+                    name = value;
+                }
+            }
+        }
+
+        public static bool Validate(String name)
+        {
+            return true;
         }
     }
 }
